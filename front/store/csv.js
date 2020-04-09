7d9802be326d1789f3dcb342ed2d62a85f371a3f
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-04-09 18:58:25 +0900

    feat : vuexの実装

--- front/store/csv.js
+++ front/store/csv.js
@@ -1,14 +1,39 @@
 export const state = () => ({
   file: null,
-  currentStep: null
+  currentStep: 1,
+  settings: []
 })
+
+const newSetting = {
+  name: null,
+  from: null,
+  converter: null
+}
 
 export const mutations = {
   setFile(state, file) {
     state.file = file
-    state.currentStep = 2
   },
   setCurrentStep(state, step) {
     state.currentStep = step
+  },
+  addSetting(state) {
+    const setting = { ...newSetting }
+    setting.id = state.settings.length + 1
+
+    state.settings.push(setting)
+  },
+  updateSetting(state, { index, key, value }) {
+    const settings = state.settings.concat()
+    settings[index][key] = value
+
+    state.settings = settings
+  }
+}
+
+export const getters = {
+  csvContent(state) {
+    const fileReader = new FileReader()
+    return fileReader.readAsText(state.csv)
   }
 }
