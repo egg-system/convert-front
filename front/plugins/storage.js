11f7eed1f9b712dab9f420487328d1e9b3abf673
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-04-10 13:01:53 +0900

    fix : バグ修正

--- front/plugins/storage.js
+++ front/plugins/storage.js
@@ -5,7 +5,7 @@ export default ({ store }) => {
     injectStorageState({
       storage: window.sessionStorage,
       key: 'csv',
-      paths: ['csv.settings', 'csv.fileKey']
+      paths: ['csv.converter.settings', 'csv.fileKey']
     })(store)
   })
 }
