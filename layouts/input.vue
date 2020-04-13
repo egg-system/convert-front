9b9095eeb24f3d710e77fb64ef7acff9564bc938
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-04-13 21:06:24 +0900

    fix : layoutの階層移行

---
+++ layouts/input.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  computed: {
+    title() {
+      return 'CSV convert'
+    }
+  }
+}
+</script>
