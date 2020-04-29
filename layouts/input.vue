b354719507c72bb4a2720273243182ebe8ffaff5
codeToshiki <aureusarca@gmail.com>
2020-04-29 19:23:00 +0900

    logo追加

--- layouts/input.vue
+++ layouts/input.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app dark>
     <v-app-bar fixed app>
+      <a href="https://eggsystem.co.jp/" target="blank">
+        <img class="logo" :src="image_src" alt="x-face" />
+      </a>
       <v-toolbar-title v-text="title" />
     </v-app-bar>
     <v-content>
@@ -15,11 +18,24 @@
 </template>
 
 <script>
+// src="~/assets/images/xfacelogo.png"
 export default {
+  data() {
+    return {
+      image_src: require('@/assets/images/xfacelogo.png')
+    }
+  },
   computed: {
     title() {
-      return 'CSV convert'
+      return 'Data Convert'
     }
   }
 }
 </script>
+
+<style scoped>
+.logo {
+  height: 20px;
+  margin-right: 10px;
+}
+</style>
