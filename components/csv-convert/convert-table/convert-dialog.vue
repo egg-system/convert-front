faa2c1083636612e058e00f3ea59a85675e6e727
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-05-05 17:37:41 +0900

    fix : 初期化の修正

--- components/csv-convert/convert-table/convert-dialog.vue
+++ components/csv-convert/convert-table/convert-dialog.vue
@@ -43,7 +43,8 @@ export default {
       return {
         name: null,
         fixedValue: null,
-        fromIndex: null
+        fromIndex: null,
+        replaceKey: null
       }
     },
     isShown: {
