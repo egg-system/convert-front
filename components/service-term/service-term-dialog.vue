09d25b45d4eb58ddfeff5f577feb53b3984397f2
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-05-06 01:04:13 +0900

    fix : 利用規約周りの修正

---
+++ components/service-term/service-term-dialog.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-dialog v-model="dialog" width="70%">
+    <template v-slot:activator="{ on }">
+      <button color="primary" dark v-on="on">
+        &ensp;利用規約
+      </button>
+    </template>
+    <v-card>
+      <v-row class="term-header">
+        <v-card-title class="term-title">
+          <h3 class="pl-10">利用規約</h3>
+        </v-card-title>
+        <v-card-actions class="pt-3">
+          <v-btn icon @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-row>
+      <v-card-text>
+        <service-term />
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import serviceTerm from './service-term'
+
+export default {
+  components: { serviceTerm },
+  data: () => ({
+    dialog: false
+  })
+}
+</script>
