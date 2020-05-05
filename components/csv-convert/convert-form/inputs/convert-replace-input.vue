fa9f7df7ac078addc46d9ca256a33b6d5df02e80
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-05-05 19:58:58 +0900

    feat : 変換設定の項目を追加

--- components/csv-convert/convert-form/inputs/convert-replace-input.vue
+++ components/csv-convert/convert-form/inputs/convert-replace-input.vue
@@ -1,25 +1,37 @@
 <template>
   <div>
     <v-row>
-      <v-radio-group
-        v-model="replaceComponent"
-        label="コード変換の設定"
-        width="100%"
-      >
-        <v-radio :value="null" label="コード変換は設定しない" />
-        <v-radio :value="true" label="固定の値にする" />
+      <v-radio-group v-model="doReplace" label="コード変換の設定" width="100%">
+        <v-radio :value="false" label="コード変換を設定しない" />
+        <v-radio :value="true" label="コード変換を設定する" />
       </v-radio-group>
     </v-row>
+    <replace-setting-input v-if="doReplace" v-model="convertSetting" />
   </div>
 </template>
 
 <script>
 import convertFormMixins from '../mixins/convert-form-mixins'
+import replaceSettingInput from './replace/replace-setting-input.vue'
 
 export default {
+  components: { replaceSettingInput },
   mixins: [convertFormMixins],
   data: () => ({
-    replaceComponent: null
-  })
+    doReplace: false
+  }),
+  watch: {
+    doReplace() {
+      if (!this.doReplace) {
+        this.convertSetting = {
+          ...this.convertSetting,
+          replaceKey: null
+        }
+      }
+    }
+  },
+  created() {
+    this.doReplace = !!this.convertSetting.replaceKey
+  }
 }
 </script>
