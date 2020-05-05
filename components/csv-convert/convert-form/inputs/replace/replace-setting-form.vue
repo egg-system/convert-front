c797480cc944b29a8f1330265d671e07aa742fbe
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-05-05 19:59:16 +0900

    feat : 置換設定周りの画面を追加

---
+++ components/csv-convert/convert-form/inputs/replace/replace-setting-form.vue
@@ -0,0 +1,104 @@
+<template>
+  <v-container>
+    <v-form ref="replaceFrom" v-model="isFormValid">
+      <v-row align="baseline">
+        <v-col>
+          <v-text-field
+            v-model="replaceName"
+            :rules="[
+              (value) => !!value || 'コード変換設定の名前を入力してください'
+            ]"
+            label="コード変換設定の名前を入力する"
+          />
+        </v-col>
+        <v-col>
+          <v-row justify="end" align="center">
+            <v-btn class="ma-2" color="primary" outlined @click="reflect">
+              反映する
+            </v-btn>
+            <v-btn class="ma-2" color="red" outlined @click="cancel">
+              キャンセル
+            </v-btn>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-container>
+        <v-row v-if="errorMessage">
+          <v-alert type="error">{{ errorMessage }}</v-alert>
+        </v-row>
+        <v-row>
+          <replace-setting-table v-model="replaceValues" />
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+import replaceSettingTable from './table/replace-setting-table'
+
+export default {
+  components: { replaceSettingTable },
+  props: {
+    value: {
+      type: Object,
+      required: true
+    }
+  },
+  data: () => ({
+    isFormValid: false,
+    errorMessage: null
+  }),
+  computed: {
+    replaceName: {
+      get() {
+        return this.value.name
+      },
+      set(replaceName) {
+        this.$emit('input', {
+          ...this.value,
+          name: replaceName
+        })
+      }
+    },
+    replaceValues: {
+      get() {
+        return this.value.value
+      },
+      set(replaceValues) {
+        this.$emit('input', {
+          ...this.value,
+          value: replaceValues
+        })
+      }
+    }
+  },
+  methods: {
+    cancel() {
+      this.$emit('input', null)
+    },
+    reflect() {
+      this.$refs.replaceFrom.validate()
+      this.validation()
+      if (!this.isFormValid || this.errorMessage) {
+        return
+      }
+
+      this.addReplace(this.value)
+      this.cancel()
+    },
+    validation() {
+      this.errorMessage = null
+
+      const isValidReplaces = this.value.value.every((value) => {
+        return value.from && value.to
+      })
+      if (!isValidReplaces) {
+        this.errorMessage = '未入力のコード変換設定があります'
+      }
+    },
+    ...mapMutations('csv/converter', ['addReplace'])
+  }
+}
+</script>
---
+++ components/csv-convert/convert-form/inputs/replace/table/replace-table-constants.js
@@ -0,0 +1,19 @@
+export const replaceHeader = [
+  {
+    value: 'state',
+    width: '15px'
+  },
+  {
+    text: '変換元のコード',
+    value: 'from'
+  },
+  {
+    text: '変換後の値',
+    value: 'to'
+  },
+  {
+    text: 'アクション',
+    value: 'action',
+    width: '15%'
+  }
+]
