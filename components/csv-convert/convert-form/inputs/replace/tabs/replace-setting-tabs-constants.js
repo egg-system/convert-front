461c60d6340acf7ad1918d56c16f26f888fbb6e5
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-06-10 05:17:54 +0900

    fix : フォルダ移動

---
+++ components/csv-convert/convert-form/inputs/replace/tabs/replace-setting-tabs-constants.js
@@ -0,0 +1,10 @@
+export const inputTabItems = [
+  {
+    name: '手動で入力する',
+    component: 'replace-setting-table'
+  },
+  {
+    name: '一括で入力する',
+    component: 'replace-setting-bulk-input'
+  }
+]
