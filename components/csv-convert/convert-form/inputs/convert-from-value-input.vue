b28873b550df69f64f3e66f7a2652dee00736c66
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-06-10 11:49:37 +0900

    fix : 各種input用コンポーネントの整理

---
+++ components/csv-convert/convert-form/inputs/convert-from-value-input.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row>
+    <v-select
+      v-model="fromIndex"
+      :items="csvHeaders"
+      label="取得元の列名を選択する"
+      :rules="[(value) => value !== null || '取得元の列名を入力してください。']"
+    />
+  </v-row>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import convertFormMixin from './mixins/convert-form-mixin'
+
+export default {
+  mixins: [convertFormMixin],
+  computed: {
+    fromIndex: {
+      get() {
+        return this.convertSetting.fromIndex
+      },
+      set(fromIndex) {
+        this.convertSetting = {
+          ...this.convertSetting,
+          fromIndex
+        }
+      }
+    },
+    ...mapGetters('csv/file', ['csvHeaders'])
+  }
+}
+</script>
